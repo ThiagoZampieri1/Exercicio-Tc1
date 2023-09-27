35213f7f57acd618b10b53c067afcd37038e718b
Thiago Zampieri <thiagozampieri@Thiagos-MacBook-Air.local>
2023-09-26 21:22:40 -0300

    feat: add testApagarLampadaB

--- src/Lista/Teste.java
+++ src/Lista/Teste.java
@@ -57,6 +57,18 @@ public class Teste extends TestCase{
 		assertEquals(retornoEsperado, retorno);
 	}
 	
+	@Test
+	public void testApagarLampadaB() {
+		int ia = 0;
+		int ib = 1;
+		int fa = 0;
+		int fb = 0;
+		
+		int retornoEsperado = 2;
+		
+		int retorno = Main.lampadas(ia, ib, fa, fb);
+		assertEquals(retornoEsperado, retorno);
+	}
 	
 	
 }
